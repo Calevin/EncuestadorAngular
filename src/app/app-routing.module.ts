ce93b75f9055c9494a3af169a97de81a54b82861
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2021-01-09 20:53:37 -0300

    Declaracion y routing componentes inicio y encuesta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { EncuestaComponent } from './paginas/encuesta/encuesta.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component: InicioComponent},
+  {path: 'encuesta', component: EncuestaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
